1568d783f1832064ade4d6a4949560b9f02cd5a9
Chirag Kashyap <chirag@Mac.lan>
2026-01-20 18:03:50 +0530

    Add edit and delete notes feature

--- main.js
+++ main.js
@@ -10,38 +10,70 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
-try{
+try {
     const conn = await mongoose.connect("mongodb://localhost:27017/allnotes")
     console.log('mongoose connected with express successfully!');
-}catch(err){
+} catch (err) {
     console.log(err);
-    
+
 }
 
 
-app.get('/', async(req, res) => {
-    let data = await note.find()
-    res.render('index',{data})
-})
-app.get('/:title', async(req, res) => {
-    let bottom = await note.findOne({title:req.params.title},{desc:1,_id:0})
-    res.render('desc',{title:req.params.title,bottom})
-})
-app.post('/create', async(req, res) => {
-    let title =  req.body.title
-    let desc =  req.body.details
-    if(title.trim()=='' || desc.trim()==''){
+app
+    .get('/', async (req, res) => {
+        let data = await note.find()
+        res.render('index', { data })
+    })
 
+    .get('/note/:id', async (req, res) => {
+        let bottom = await note.findOne({ _id: req.params.id}, {title:1, desc: 1})
+        res.render('desc', { title: bottom.title, bottom })
+    })
+
+    .get('/note/edit/:id', async (req, res) => {
+        let bottom = await note.findOne({ _id: req.params.id }, {title:1, desc: 1})
+        res.render('edit', { title: bottom.title, bottom })
+    })
+
+    .post('/create', async (req, res) => {
+        let title = req.body.title
+        let desc = req.body.details
+        if (title.trim() == '' || desc.trim() == '') {
+
+            res.redirect('/')
+        }
+        else {
+            await note.create({
+                title: title,
+                desc: desc
+            })
+            res.redirect('/')
+        }
+    })
+
+    .post('/note/edit/:id', async (req, res) => {
+        let newTitle = req.body.title
+        let newDesc = req.body.details
+        if (newTitle.trim() == '' || newDesc.trim() == '') {
+
+            res.redirect(`/note/edit/${req.params.id}`)
+        }
+        else {
+            await note.updateOne({
+                _id:req.params.id
+            }
+                ,{
+                title:newTitle,
+                desc:newDesc
+            })
+            res.redirect('/')
+        }
+    })
+
+    .post('/note/delete/:id', async (req, res) => {
+        await note.deleteOne({_id:req.params.id})
         res.redirect('/')
-    }
-    else{
-        await note.create({
-            title: title,
-            desc: desc
-        })
-        res.redirect('/')
-    }
-})
+    })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
